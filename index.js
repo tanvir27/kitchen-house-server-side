13a15ed0e2ac58fe3c39390faf468511b79df9c9
Tanvir Hasan <87339504+tanvir27@users.noreply.github.com>
2023-05-04 15:02:30 +0600

    server side uploaded

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const cors = require("cors");
+const allData = require("./data/chefInfo.json");
+
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 5000;
+
+// all data
+app.get("/", (req, res) => {
+  res.send("Hello From kitchen house server");
+});
+app.get("/chefDetails", (req, res) => {
+  res.send(allData);
+});
+// specific data
+app.get("/chefDetails/:id", (req, res) => {
+  const id = req.params.id;
+  const specificData = allData.find((data) => data.id == id);
+  res.send(specificData);
+});
+
+app.listen(port, () => {
+  console.log(`Listening from port: ${port}`);
+});
